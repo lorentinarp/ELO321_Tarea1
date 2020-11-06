a1b10e0ee2cd4cdb90e18e66bc675305f7112b5f
CamiloDonoso <camilo.donoso@sansano.usm.cl>
2020-11-06 18:03:18 -0300

    Se agrega las variables globales y un arreglo con 50 numeros aleatorios

--- codB.c
+++ codB.c
@@ -1,1 +1,17 @@
-/*Hola mundo*/
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+// Variables globales
+float avg_value;
+int min_value;
+int max_value;
+int numArray[50];
+
+int main(){
+    for (int i = 0 ; i<50 ; i++ ){
+        numArray[i] = rand() % 100;
+        printf("numArray[%i] = %i\n", i,numArray[i]);
+    }
+    return 0;
+}
