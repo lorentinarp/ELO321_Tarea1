4d78c04cd012041ad51ed803276f272e96aee342
CamiloDonoso <camilo.donoso@sansano.usm.cl>
2020-11-06 20:21:20 -0300

    Se termina la pregunta 3 de la parte B

--- codB_P3.c
+++ codB_P3.c
@@ -4,7 +4,7 @@
               Camilo Donoso Collao
 *@date      : 06/11/202
 *@brief     : Código para tarea 01 de ELO321, semestre 2020 -2
-              Corresponde a la parte B, las preguntas 0,1 y 2.
+              Corresponde a la parte B, pregunta 3.
 */
 
 #include <stdio.h>
@@ -39,73 +39,74 @@ int main()
   pthread_attr_t attr[3];
 
   // El primer hilo calcula el valor promedio
-//   pthread_attr_init(&attr[0]);
-//   pthread_create(&threadID[0], &attr[0], return_Avg(&avg_value), &threadID[0]);
+  pthread_attr_init(&attr[0]);
+  pthread_create(&threadID[0], &attr[0], return_Avg, &avg_value);
     
-//   // El segundo hilo busca el valor minimo
+  // El segundo hilo busca el valor minimo
   pthread_attr_init(&attr[1]);
   pthread_create(&threadID[1], &attr[1], return_Min, &min_value);
 
-//   // El tercer hijo busca el valor maximo
-//   pthread_attr_init(&attr[2]);
-//   pthread_create(&threadID[2], &attr[1], return_Max(&max_value), &threadID[2]);
+  // El tercer hijo busca el valor maximo
+  pthread_attr_init(&attr[2]);
+  pthread_create(&threadID[2], &attr[1], return_Max, &max_value);
 
   // El proceso padre espera a todos los hilos
-//   for (int i = 0 ; i < 3 ; i++ ){
-//     pthread_join(threadID[i], NULL);
-//   }
-  
-  pthread_join(threadID[0], NULL);
+  for (int i = 0 ; i < 3 ; i++ ){
+    pthread_join(threadID[i], NULL);
+  }
+  pthread_join(threadID[1], NULL);
   // Se imprimen los valores
   printf("Valor promedio = %f\n", avg_value);
-//   printf("Valor mínimo = %i\n", min_value);
-//   printf("Valor máximo = %i\n", max_value);
+  printf("Valor mínimo = %i\n", min_value);
+  printf("Valor máximo = %i\n", max_value);
 
   return 0;
 }
 
 
-// void* return_Avg(float *arg){
-//     printf("Hola\n");
-//   int tmp = 0;
-//   // Se calcula la suma de todos los elementos del arreglo
-//   for (int i = 0 ; i < 50 ; i++){
-//     tmp = tmp + numArray[i];
-//   }
-//   // Se utilizó mutex cuando los hilos modifican las variables globales
-//   pthread_mutex_lock(&lock); // Entry section
-//   // Critical section
-//   (*arg) = tmp/50.0; // Se calcula el promedio
-//   pthread_mutex_unlock(&lock); // Exit section
-//   // Remainder section
-//   printf("Thread ID: %lu calculó promedio\n",pthread_self());
-// }
+void* return_Avg(void *arg){
+  int tmp = 0;
+  float *value = arg;
+  // Se calcula la suma de todos los elementos del arreglo
+  for (int i = 0 ; i < 50 ; i++){
+    tmp = tmp + numArray[i];
+  }
+  // Se utilizó mutex cuando los hilos modifican las variables globales
+  pthread_mutex_lock(&lock); // Entry section
+  // Critical section
+  (*value) = tmp/50.0; // Se calcula el promedio
+  pthread_mutex_unlock(&lock); // Exit section
+  // Remainder section
+  printf("Thread ID: %lu calculó promedio\n",pthread_self());
+}
 
 void* return_Min(void *arg){
   int tmp = numArray[0];
+  int *value = arg;
   // Ciclo for para buscar el valor mínimo del arreglo
   for (int i = 0 ; i < 50 ; i++){
     if (tmp > numArray[i]) tmp = numArray[i];
   }
   pthread_mutex_lock(&lock); // Entry section
   // Critical section
-  (*arg) = tmp;
+  (*value) = tmp;
   pthread_mutex_unlock(&lock); // Exit section
   // Remainder section
   printf("Thread ID: %lu calculó valor mínimo\n",pthread_self());
 }
 
-// void* return_Max(int *arg){
-//   int tmp = numArray[0];
-//   // Ciclo for para buscar el valor máximo del arreglo
-//   for (int i = 0 ; i < 50 ; i++){
-//     if (tmp < numArray[i]) tmp = numArray[i];
-//   }
-//   pthread_mutex_lock(&lock);  // Entry section
-//   // Critical section
-//   (*arg) = tmp;
-//   pthread_mutex_unlock(&lock);  // Exit section
-//   // Remainder section
-//   printf("Thread ID: %lu calculó valor máximo\n",pthread_self());
-// }
+void* return_Max(void *arg){
+  int tmp = numArray[0];
+  int *value = arg;
+  // Ciclo for para buscar el valor máximo del arreglo
+  for (int i = 0 ; i < 50 ; i++){
+    if (tmp < numArray[i]) tmp = numArray[i];
+  }
+  pthread_mutex_lock(&lock);  // Entry section
+  // Critical section
+  (*value) = tmp;
+  pthread_mutex_unlock(&lock);  // Exit section
+  // Remainder section
+  printf("Thread ID: %lu calculó valor máximo\n",pthread_self());
+}
 
